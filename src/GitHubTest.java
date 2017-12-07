6274a76058c45b327c19607dd3eb34b2b1473498
mttmo <mttmo@iscte-iul.pt>
2017-12-07 15:10:18 +0000

    Second commit

--- src/GitHubTest.java
+++ src/GitHubTest.java
@@ -4,7 +4,7 @@ public class GitHubTest {
 	public static void main(String[] args) {
 		for(int i = 0; i < 10; i++) { 
 			try {
-				System.out.println("Hello GitHub " + i + " this is my first test");
+				System.out.println("Hello GitHub " + i + " this is my second test");
 				Thread.currentThread().sleep(500);
 			} catch (InterruptedException e) {
 				// TODO Auto-generated catch block
