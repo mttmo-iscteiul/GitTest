a5429d510b87cd4f388a84207e60d0ed3f23ea8c
mttmo <mttmo@iscte-iul.pt>
2017-12-07 14:52:09 +0000

    First Commit

---
+++ src/GitHubTest.java
@@ -0,0 +1,15 @@
+
+public class GitHubTest {
+
+	public static void main(String[] args) {
+		for(int i = 0; i < 10; i++) { 
+			try {
+				System.out.println("Hello GitHub " + i + " this is my first test");
+				Thread.currentThread().sleep(500);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
+}
